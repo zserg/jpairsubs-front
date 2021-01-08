518fa7aaf3e130d0db0835765192a975d505f4ff
Sergey Zadyabin <s.zadyabin@crestwavetech.ru>
2021-01-08 17:07:20 +0300

    run some tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, { Fragment } from 'react';
+import axios from 'axios';
+import { Container, Row, Col } from 'reactstrap';
+
+import Post from './components/Post';
+import Header from './components/Header';
+import SideCard from './components/SideCard';
+
 import logo from './logo.svg';
 import './App.css';
+const App = () => (
+  <Fragment>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Header />
+
+    <main className="my-5 py-5">
+      <Container className="px-0">
+        <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
+
+          <Col xs={{ order: 2 }} md={{ size: 4, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
+            <SideCard />
+          </Col>
+
+          <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
+            <Post />
+          </Col>
+
+        </Row>
+      </Container>
+    </main>
+
+  </Fragment>
+);
+
 
 export default App;
