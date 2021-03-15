cce51ee0d6039d4295c0c1eff8d10704f1470f17
Sergey Zadyabin <s.zadyabin@crestwavetech.ru>
2021-03-15 18:07:38 +0300

    ...

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,7 @@ import { Badge } from 'reactstrap';
 
 class Post extends Component {
 
-  state = { post: null }
+  state = { post: [] }
 
   componentDidMount() {
     axios.get('http://s4.zserg.net:8089/api/v1/movies')
@@ -23,7 +23,9 @@ class Post extends Component {
 
           <span className="d-block pb-4 h2 text-dark border-bottom border-gray">Getting Started with React !!!</span>
 
-          <article className="pt-5 text-secondary text-justify" style={{ fontSize: '0.9rem', whiteSpace: 'pre-line' }}>{this.state.post}</article>
+          <article className="pt-5 text-secondary text-justify" style={{ fontSize: '0.9rem', whiteSpace: 'pre-line' }}>
+             {this.state.post.map(movie => <div>{movie.title}</div>)}
+          </article>
 
         </div> }
       </Fragment>
