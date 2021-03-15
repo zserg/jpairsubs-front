614cbed1e5440a6c9bfab0754fd51b46e64bb205
Sergey Zadyabin <s.zadyabin@crestwavetech.ru>
2021-03-15 17:31:01 +0300

    ...

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,7 @@ class Post extends Component {
   state = { post: null }
 
   componentDidMount() {
-    axios.get('http://localhost:8081/api/v1/movies')
+    axios.get('http://s4.zserg.net:8089/api/v1/movies')
       .then(response => this.setState({ post: response.data }));
   }
 
@@ -17,11 +17,11 @@ class Post extends Component {
         { this.state.post && <div className="position-relative">
 
           <span className="d-block pb-2 mb-0 h6 text-uppercase text-info font-weight-bold">
-            Editor's Pick
+            Editor's Pick (Movies)
             <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>New</Badge>
           </span>
 
-          <span className="d-block pb-4 h2 text-dark border-bottom border-gray">Getting Started with React</span>
+          <span className="d-block pb-4 h2 text-dark border-bottom border-gray">Getting Started with React !!!</span>
 
           <article className="pt-5 text-secondary text-justify" style={{ fontSize: '0.9rem', whiteSpace: 'pre-line' }}>{this.state.post}</article>
 
